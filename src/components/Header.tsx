497fe62262ea5d9f2a1316ea2116222b5aa2657b
rubuz <rubuz23@gmail.com>
2024-03-13 14:50:41 +0100

    Update Header component styling

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,9 +4,11 @@ const Header = () => {
       <div className="absolute left-0 top-1/2 -translate-y-1/2">
         <p>LOGO</p>
       </div>
-      <div className="flex w-fit rounded-2xl bg-light-yellow px-10 py-4">
+      <div className="flex w-fit rounded-2xl bg-light-yellow px-10 py-2">
         <ul className="flex gap-10">
-          <li>Home</li>
+          <li className="cursor-pointer rounded-xl p-2 transition-all duration-200 hover:-translate-y-0.5 hover:font-semibold">
+            Home
+          </li>
           <li>About</li>
           <li>Contact</li>
           <li>Contact</li>
