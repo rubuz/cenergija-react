489d62defb7e19b950c9e68628728fe0d10342f3
rubuz <rubuz23@gmail.com>
2024-03-26 15:12:46 +0100

    center info working

--- src/App.tsx
+++ src/App.tsx
@@ -8,13 +8,8 @@ function App() {
       <div className="h-[100dvh] bg-white font-roboto ">
         <Header />
         <Section>
-          <h1 className="mt-20">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, quos
-            quo perspiciatis magnam omnis officia voluptatibus ut, veritatis
-            laborum harum iusto suscipit enim commodi! Aliquam itaque ex qui
-            molestias officiis totam non. Ullam dolore labore, distinctio
-            provident magni corporis sed sit blanditiis earum ad praesentium
-            voluptatibus magnam minus cumque eum.
+          <h1 className="my-14 text-center text-5xl font-bold">
+            CENA ELEKTRIČNE ENERGIJE
           </h1>
         </Section>
         <Section>
