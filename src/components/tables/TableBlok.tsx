ba52841b00d3d2320459b5aa0a412a0d905f2723
Matjaz Lovenjak <rubuz23@gmail.com>
2024-04-03 18:16:52 +0200

    Refactor TableBlok component styles

--- src/components/tables/TableBlok.tsx
+++ src/components/tables/TableBlok.tsx
@@ -2,14 +2,14 @@ const TableBlok = () => {
   return (
     <div className="flex flex-col">
       <div className="grid grid-cols-[repeat(2,10%)_repeat(5,_1fr)] grid-rows-[20%_repeat(2,minmax(80px,_1fr))] gap-[2px] [&>*]:rounded-sm">
-        <div className="grid_top_row bg-table-grey-dark col-span-2 !rounded-tl-2xl">
+        <div className="grid_top_row col-span-2 !rounded-tl-2xl bg-table-grey-dark">
           obdobje
         </div>
         <div className="grid_top_row bg-table-red">blok 1</div>
         <div className="grid_top_row bg-table-yellow">blok 2</div>
         <div className="grid_top_row bg-table-cream">blok 3</div>
         <div className="grid_top_row bg-table-greeny">blok 4</div>
-        <div className="grid_top_row bg-table-green !rounded-tr-2xl">
+        <div className="grid_top_row !rounded-tr-2xl bg-table-green">
           blok 5
         </div>
         <div className="grid_h_item row-span-2 text-3xl font-bold">
@@ -81,7 +81,7 @@ const TableBlok = () => {
           <p>14:00 - 16:00</p>
           <p>20:00 - 22:00</p>
         </div>
-        <div className="grid_inner_item bg-table-green !rounded-br-2xl">
+        <div className="grid_inner_item !rounded-br-2xl bg-table-green">
           <p>0:00 - 6:00</p>
           <p>22:00 - 24:00</p>
         </div>
