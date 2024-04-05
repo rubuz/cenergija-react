4a9efec26180fd7ac69200143ca1daa7a3044cec
rubuz <rubuz23@gmail.com>
2024-04-05 15:11:57 +0200

    Update Header component styling and navigation

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -18,55 +18,57 @@ const Header = () => {
 
   return (
     <div
-      className={`navbar fixed left-0 top-0 z-50 mx-auto transition-all duration-200 ease-in-out ${isScrolled ? " bg-white/70 shadow-md backdrop-blur-lg" : ""}`}
+      className={`fixed left-0 top-0 z-50 w-full transition-all duration-200 ease-in-out ${isScrolled ? " bg-white/70 shadow-md backdrop-blur-lg" : ""}`}
     >
-      <div className="navbar-start flex gap-3">
-        <img src={Logo} alt="Cenergija" className="h-12 w-12" />
-        <a className="text-xl font-semibold">
-          <span className="text-table-red">cEne</span>rgija
-        </a>
-      </div>
-      <div className="navbar-center">
-        <h1 className="text-center text-4xl font-bold">
-          TRENUNTA CENA ELEKTRIČNE OMREŽNINE
-        </h1>
-      </div>
-      <div className="navbar-end">
-        <div className="dropdown dropdown-end">
-          <div
-            tabIndex={0}
-            role="button"
-            className="group btn btn-ghost rounded-xl  border-transparent hover:border-transparent hover:bg-table-yellow"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-7 w-7 transition-all duration-200 group-hover:scale-110"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
+      <div className={`navbar mx-auto max-w-[1440px]`}>
+        <div className="navbar-start flex gap-3">
+          <img src={Logo} alt="Cenergija" className="h-12 w-12" />
+          <a className="text-2xl font-semibold">
+            <span className="text-table-red">cEne</span>rgija
+          </a>
+        </div>
+        <div className="navbar-center">
+          <h1 className="text-center text-4xl font-bold">
+            TRENUNTA CENA ELEKTRIČNE OMREŽNINE
+          </h1>
+        </div>
+        <div className="navbar-end">
+          <div className="dropdown dropdown-end">
+            <div
+              tabIndex={0}
+              role="button"
+              className="group btn btn-ghost rounded-xl  border-transparent hover:border-transparent hover:bg-table-yellow"
             >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h16M4 18h7"
-              />
-            </svg>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-7 w-7 transition-all duration-200 group-hover:scale-110"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h16M4 18h7"
+                />
+              </svg>
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu dropdown-content menu-lg z-[1] mt-3 w-52 rounded-box bg-base-100 p-2 shadow"
+            >
+              <li>
+                <a>Homepage</a>
+              </li>
+              <li>
+                <a>Portfolio</a>
+              </li>
+              <li>
+                <a>About</a>
+              </li>
+            </ul>
           </div>
-          <ul
-            tabIndex={0}
-            className="menu dropdown-content menu-lg z-[1] mt-3 w-52 rounded-box bg-base-100 p-2 shadow"
-          >
-            <li>
-              <a>Homepage</a>
-            </li>
-            <li>
-              <a>Portfolio</a>
-            </li>
-            <li>
-              <a>About</a>
-            </li>
-          </ul>
         </div>
       </div>
     </div>
